2620e4e5e5f2f2dcd831d10d8a2811faff679edb
Igorlo <9440533@gmail.com>
2019-04-25 19:38:46 +0300

    Welcoming text at the start

--- src/ConsoleApplication.kt
+++ src/ConsoleApplication.kt
@@ -1,5 +1,5 @@
-import Utilites.Companion.getUserIntParameter
-import Utilites.Companion.printResultSet
+import Utilities.Companion.getUserIntParameter
+import Utilities.Companion.printResultSet
 
 import java.util.*
 
@@ -9,16 +9,22 @@ private val connector: DBConnector = DBConnector()
 
 fun main(args: Array<String>) {
     println(
-        "Welcome to the data generator for Postgres DB. This program will\n" +
-                "ask to input some parameters required for data generation. After\n" +
-                "collecting all required data it will connect to selected DB and\n" +
-                "fill tables with random data."
+        "\n\n" +
+                "\t----------------------------------------------------------------\n" +
+                "\tWelcome to the data generator for Postgres DB. This program will\n" +
+                "\task to input some parameters required for data generation. After\n" +
+                "\tcollecting all required data it will connect to selected DB and\n" +
+                "\tfill tables with random data.\n\n" +
+                "\t\t   Created by Igor Lopatinskiy for university project.\n" +
+                "\t\t\t\t\t\t\t   Spring 2019\n" +
+                "\t\t\t------------------------------------------------\n\n"
     )
 //    val first = getUserIntParameter("first parameter (int):")
 //    val second = getUserIntParameter("second parameter (int):")
 //    val third = getUserIntParameter("third parameter (int):")
 
     connector.newConnection()
+
     val selectResult = connector.getResultSetOfSelect("characters")
     printResultSet(selectResult, 15)
 
--- src/Utilites.kt
+++ src/Utilites.kt
@@ -1,10 +1,15 @@
+import org.slf4j.LoggerFactory
 import java.sql.ResultSet
 import java.util.*
 
-class Utilites {
+class Utilities {
 
     companion object {
+
+        private val logger = LoggerFactory.getLogger(Utilities::class.java)
+
         fun printResultSet(resultSet: ResultSet, valueMaxSize: Int = 10) {
+            logger.info("Printing ResultSet")
             val metaData = resultSet.metaData
             for (i in 1..metaData.columnCount) {
                 System.out.print(metaData.getColumnName(i).take(valueMaxSize).padEnd(valueMaxSize))
@@ -27,6 +32,7 @@ class Utilites {
         }
 
         fun getUserIntParameter(text: String): Int {
+            logger.info("Getting INT from user")
             var parameter: Int? = null
             while (parameter == null) {
                 print("$text ")
